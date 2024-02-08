b55ec3f03c6d94a667fdf5170712135b629da814
fumiko <2394425147@qq.com>
2024-02-08 22:28:18 +0800

    refactor: Move CullingType.cs to Data

--- CullFactory/Config.cs
+++ CullFactory/Config.cs
@@ -21,6 +21,7 @@ public sealed class Config
                                  "Portal occlusion culling tests what rooms are visible to the camera based on the size of the passages between them." +
                                  "This is the recommended setting, as it should yield a significant performance gain on large maps without any visual change.\n" +
                                  "Depth culling hides rooms based on the number of rooms separating them from the camera.");
+
         Culler.SettingChanged += (_, _) => Plugin.CreateCullingHandler();
 
         UpdateFrequency = configFile.Bind("General",
