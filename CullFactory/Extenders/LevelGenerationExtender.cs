230090b4f98b8f750591f892bdc64439480caef7
Zaggy1024 <Zaggy1024@gmail.com>
2024-03-04 13:00:30 -0600

    feat: Move hook for the end of a round to `PassTimeToNextDay`
    This runs on a timer a bit later than `DespawnPropsAtEndOfRound`, so it should trigger after the networked objects have been destroyed on all clients.

--- CullFactory/Extenders/LevelGenerationExtender.cs
+++ CullFactory/Extenders/LevelGenerationExtender.cs
@@ -1,31 +1,29 @@
-using System.Collections.Generic;
-using System.Reflection;
-using System.Reflection.Emit;
 using CullFactory.Behaviours.CullingMethods;
 using CullFactory.Data;
-using DunGen;
 using HarmonyLib;
-using UnityEngine;
 
 namespace CullFactory.Extenders;
 
-[HarmonyPatch(typeof(RoundManager))]
 public sealed class LevelGenerationExtender
 {
     [HarmonyPostfix]
-    [HarmonyPatch(nameof(RoundManager.waitForMainEntranceTeleportToSpawn))]
+    [HarmonyPatch(typeof(RoundManager), nameof(RoundManager.waitForMainEntranceTeleportToSpawn))]
     private static void OnLevelGenerated()
     {
         DungeonCullingInfo.OnLevelGenerated();
 
         CullingMethod.Initialize();
         Plugin.CreateCullingVisualizers();
+
+        DynamicObjects.CollectAllTrackedObjects();
     }
 
     [HarmonyPostfix]
-    [HarmonyPatch(nameof(RoundManager.DespawnPropsAtEndOfRound))]
-    private static void OnPropsDespawned()
+    [HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.PassTimeToNextDay))]
+    private static void OnRoundEnded()
     {
+        DungeonCullingInfo.ClearAll();
+
         DynamicObjects.CollectAllTrackedObjects();
     }
 }
