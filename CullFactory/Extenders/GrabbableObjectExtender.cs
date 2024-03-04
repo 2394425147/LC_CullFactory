4103c2393630cb6c0cdc83fb29bfc6a987209041
Zaggy1024 <Zaggy1024@gmail.com>
2024-03-04 12:44:15 -0600

    feat: Update known items when items are dropped
    ...and move the hooks into their own file.

---
+++ CullFactory/Extenders/GrabbableObjectExtender.cs
@@ -0,0 +1,21 @@
+﻿using CullFactory.Data;
+using HarmonyLib;
+
+namespace CullFactory.Extenders;
+
+internal class GrabbableObjectExtender
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(GrabbableObject), nameof(GrabbableObject.Start))]
+    private static void GrabbableObjectStarted(GrabbableObject __instance)
+    {
+        DynamicObjects.RefreshGrabbableObject(__instance);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(GrabbableObject), nameof(GrabbableObject.EnablePhysics))]
+    private static void GrabbableObjectDropped(GrabbableObject __instance)
+    {
+        DynamicObjects.RefreshGrabbableObject(__instance);
+    }
+}
