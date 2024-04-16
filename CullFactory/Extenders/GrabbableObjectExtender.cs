9fba2722477f90106b7f7e08ef668827b952869c
Zaggy1024 <Zaggy1024@gmail.com>
2024-04-15 21:17:33 -0500

    refactor: Use HarmonyPatch(type) on the GrabbableObjectExtender class

--- CullFactory/Extenders/GrabbableObjectExtender.cs
+++ CullFactory/Extenders/GrabbableObjectExtender.cs
@@ -1,26 +1,27 @@
-﻿using CullFactory.Data;
+using CullFactory.Data;
 using HarmonyLib;
 
 namespace CullFactory.Extenders;
 
+[HarmonyPatch(typeof(GrabbableObject))]
 internal class GrabbableObjectExtender
 {
     [HarmonyPostfix]
-    [HarmonyPatch(typeof(GrabbableObject), nameof(GrabbableObject.Start))]
+    [HarmonyPatch(nameof(GrabbableObject.Start))]
     private static void GrabbableObjectStarted(GrabbableObject __instance)
     {
         DynamicObjects.RefreshGrabbableObject(__instance);
     }
 
     [HarmonyPostfix]
-    [HarmonyPatch(typeof(GrabbableObject), nameof(GrabbableObject.EnablePhysics))]
+    [HarmonyPatch(nameof(GrabbableObject.EnablePhysics))]
     private static void GrabbableObjectDropped(GrabbableObject __instance)
     {
         DynamicObjects.RefreshGrabbableObject(__instance);
     }
 
     [HarmonyPostfix]
-    [HarmonyPatch(typeof(GrabbableObject), nameof(GrabbableObject.EnableItemMeshes))]
+    [HarmonyPatch(nameof(GrabbableObject.EnableItemMeshes))]
     private static void GrabbableObjectShownOrHidden(GrabbableObject __instance)
     {
         DynamicObjects.RefreshGrabbableObject(__instance);
