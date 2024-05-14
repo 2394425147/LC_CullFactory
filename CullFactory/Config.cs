8b670a5d65dc2ddae07479a3d3345ad43a2d50bc
Zaggy1024 <Zaggy1024@gmail.com>
2024-05-14 13:44:20 -0500

    chore: Add Wesley's yet-to-be-released interior flow to the blacklist
    It will at least initially be problematic, likely even with fallback portals enabled. For now, just disable culling and hope that performance on large maps will not suffer.

--- CullFactory/Config.cs
+++ CullFactory/Config.cs
@@ -26,7 +26,7 @@ public static class Config
     private static readonly string VersionFile =
         Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty, "version");
 
-    private static readonly string[] DefaultFlowsToBlockCulling = [];
+    private static readonly string[] DefaultFlowsToBlockCulling = ["GrandArmoryFlow"];
 
     private static readonly string[] BaseSetOfInteriorsToUseFallbackPortals = ["BunkerFlow", "School"];
 
