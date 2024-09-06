f97c78d5556548bef90893168622eccfb98caec3
Zaggy1024 <Zaggy1024@gmail.com>
2024-09-06 13:05:18 -0500

    fix: Resolve Burst methods to their managed pointers on failure
    This fixes an error where a client could not join a host in LAN.

--- CullFactory/Extenders/BurstErrorPrevention.cs
+++ CullFactory/Extenders/BurstErrorPrevention.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
 using Unity.Burst;
+using Unity.Burst.LowLevel;
 
 namespace CullFactory.Extenders;
 
@@ -44,13 +45,55 @@ internal static class BurstErrorPrevention
     [HarmonyPatch(typeof(BurstCompiler), nameof(BurstCompiler.Compile), [typeof(object), typeof(MethodInfo), typeof(bool), typeof(bool)])]
     private static IEnumerable<CodeInstruction> PatchCompile(IEnumerable<CodeInstruction> instructions)
     {
+        //   if (Options.EnableBurstCompilation && BurstCompilerHelper.IsBurstGenerated)
+        //   {
+        //       ptr = BurstCompilerService.GetAsyncCompiledAsyncDelegateMethod(BurstCompilerService.CompileAsyncDelegateMethod(delegateObj, string.Empty));
+        //   }
+        // - else
+        // + if (ptr == null)
+        //   {
+        //       if (isILPostProcessing)
+        //           return null;
+        //       GCHandle.Alloc(@delegate);
+        //       ptr = (void*)Marshal.GetFunctionPointerForDelegate(@delegate);
+        //   }
         // - if (ptr == null)
-        // - {
         // -     throw new InvalidOperationException($"Burst failed to compile the function pointer `{methodInfo}`");
-        // - }
         //   return ptr;
-        var matcher = new CodeMatcher(instructions)
-            .MatchForward(false, [
+        var matcher = new CodeMatcher(instructions);
+
+        var asyncCompileMethod = typeof(BurstCompilerService).GetMethod(nameof(BurstCompilerService.GetAsyncCompiledAsyncDelegateMethod), [typeof(int)]);
+        if (asyncCompileMethod == null)
+        {
+            Plugin.LogError("Failed to find the Burst async compilation method");
+            return instructions;
+        }
+
+        // Replace else with null check.
+        matcher
+            .MatchForward(true, [
+                new(OpCodes.Call, asyncCompileMethod),
+                new(OpCodes.Stloc_0),
+            ])
+            .Advance(1);
+        if (matcher.IsInvalid || !matcher.Instruction.Branches(out var returnLabel))
+        {
+            Plugin.LogAlways($"{matcher.Instruction}");
+            Plugin.LogError("Failed to find the call to the Burst async compilation method");
+            return instructions;
+        }
+        matcher
+            .RemoveInstruction()
+            .InsertAndAdvance([
+                    new(OpCodes.Ldloc_0),
+                    new(OpCodes.Ldc_I4_0),
+                    new(OpCodes.Conv_U),
+                    new(OpCodes.Bne_Un, returnLabel),
+                ]);
+
+        // Remove the throw statement.
+        var exceptionMatchers = new CodeMatch[]
+            {
                 new(OpCodes.Ldloc_0),
                 new(OpCodes.Ldc_I4_0),
                 new(OpCodes.Conv_U),
@@ -61,16 +104,16 @@ internal static class BurstErrorPrevention
                 new(OpCodes.Call, typeof(string).GetMethod(nameof(string.Format), [typeof(string), typeof(object)])),
                 new(OpCodes.Newobj, typeof(InvalidOperationException).GetConstructor([typeof(string)])),
                 new(OpCodes.Throw),
-            ]);
+            };
+        matcher.MatchForward(false, exceptionMatchers);
         if (matcher.IsInvalid)
         {
             Plugin.LogError("Failed to find Burst compilation failure throw statement");
             return instructions;
         }
-
         var labels = matcher.Instruction.labels;
         matcher
-            .RemoveInstructions(9)
+            .RemoveInstructions(exceptionMatchers.Length)
             .Labels.AddRange(labels);
 
         return matcher.Instructions();
