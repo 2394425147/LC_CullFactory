bf51b68ec4c33d75e4386931267535c6534812e0
Zaggy1024 <Zaggy1024@gmail.com>
2024-09-07 16:39:09 -0500

    fix: Check if items are in the interior using their target position
    When items are removed from the bag, only their targetFloorPosition is updated based on where they will drop to the ground. This means that their actual transform position will be out of bounds, and they'll always be considered outside when DynamicObjects.RefreshGrabbableObject() is called.
    
    Fixes #23

--- CullFactory/Data/DynamicObjects.cs
+++ CullFactory/Data/DynamicObjects.cs
@@ -95,10 +95,7 @@ public static class DynamicObjects
             {
                 // GrabbableObject.isInFactory is not reliable for items that are in the ship
                 // at the start of the game.
-                if (item.GetComponentInChildren<ScanNodeProperties>() is { } scanNode)
-                    isInInterior = IsInInterior(scanNode.transform.position);
-                else
-                    isInInterior = IsInInterior(item.transform.position);
+                isInInterior = IsInInterior(item.targetFloorPosition);
             }
             else
             {
