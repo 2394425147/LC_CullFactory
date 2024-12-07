647668559e04dec954519d7092a347584b6c875f
Zaggy1024 <Zaggy1024@gmail.com>
2024-12-07 14:50:10 -0600

    refactor: Use a namespace declaration in LightUtility

--- CullFactory/Services/LightUtility.cs
+++ CullFactory/Services/LightUtility.cs
@@ -4,61 +4,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
 
-namespace CullFactory.Services
+namespace CullFactory.Services;
+
+public static class LightUtility
 {
-    public static class LightUtility
+    public static bool Affects(this Light light, Bounds bounds)
     {
-        public static bool Affects(this Light light, Bounds bounds)
-        {
-            var lightRange = light.range;
-            var lightDistanceSquared = bounds.SqrDistance(light.transform.position);
-            if (lightDistanceSquared > lightRange * lightRange)
-                return false;
-            if (light.type == LightType.Spot)
-                return Geometry.SpotLightInfluencesBounds(light, bounds);
-            return true;
-        }
-
-        public static bool Affects(this Light light, TileContents tile)
-        {
-            return light.Affects(tile.rendererBounds);
-        }
-
-        public static bool Affects(this Light light, IEnumerable<TileContents> tiles)
-        {
-            foreach (var tile in tiles)
-            {
-                if (light.Affects(tile))
-                    return true;
-            }
+        var lightRange = light.range;
+        var lightDistanceSquared = bounds.SqrDistance(light.transform.position);
+        if (lightDistanceSquared > lightRange * lightRange)
             return false;
-        }
+        if (light.type == LightType.Spot)
+            return Geometry.SpotLightInfluencesBounds(light, bounds);
+        return true;
+    }
 
-        public static bool HasShadows(this Light light)
+    public static bool Affects(this Light light, TileContents tile)
+    {
+        return light.Affects(tile.rendererBounds);
+    }
+
+    public static bool Affects(this Light light, IEnumerable<TileContents> tiles)
+    {
+        foreach (var tile in tiles)
         {
-            return light.shadows != LightShadows.None;
+            if (light.Affects(tile))
+                return true;
         }
+        return false;
+    }
 
-        public static bool PassesThroughOccluders(this Light light)
-        {
-            return !light.HasShadows() || light.GetComponent<HDAdditionalLightData>() is { shadowDimmer: < 1 };
-        }
+    public static bool HasShadows(this Light light)
+    {
+        return light.shadows != LightShadows.None;
+    }
 
-        public static void SetVisible(this Light light, bool visible)
-        {
-            if (light == null)
-                return;
+    public static bool PassesThroughOccluders(this Light light)
+    {
+        return !light.HasShadows() || light.GetComponent<HDAdditionalLightData>() is { shadowDimmer: < 1 };
+    }
 
-            if (light.cullingMask != -1 && light.cullingMask != 0)
-                Plugin.LogWarning($"Light {light.name}'s culling mask was an unexpected value of {light.cullingMask}.");
+    public static void SetVisible(this Light light, bool visible)
+    {
+        if (light == null)
+            return;
 
-            light.cullingMask = visible ? -1 : 0;
-        }
+        if (light.cullingMask != -1 && light.cullingMask != 0)
+            Plugin.LogWarning($"Light {light.name}'s culling mask was an unexpected value of {light.cullingMask}.");
 
-        public static void SetVisible(this IEnumerable<Light> lights, bool visible)
-        {
-            foreach (var light in lights)
-                light.SetVisible(visible);
-        }
+        light.cullingMask = visible ? -1 : 0;
+    }
+
+    public static void SetVisible(this IEnumerable<Light> lights, bool visible)
+    {
+        foreach (var light in lights)
+            light.SetVisible(visible);
     }
 }
